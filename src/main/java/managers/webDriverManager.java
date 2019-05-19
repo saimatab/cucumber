1e0bfe541aac4743f68133d05f72c68889efdc4a
Haris Saleem <harissaleem@Wunders-MacBook-Pro-2.local>
2019-05-19 13:47:04 +0200

    Fixed issues with running tests. Now tests can run on the same browser session but that is not how you should maintain a BDD test suite.

--- src/main/java/managers/webDriverManager.java
+++ src/main/java/managers/webDriverManager.java
@@ -1,64 +1,59 @@
 package managers;
 
 
-
-
 import java.util.concurrent.TimeUnit;
+import DataProviders.ConfigFileReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-import DataProviders.ConfigFileReader;
 
- 
 public class webDriverManager {
- private WebDriver driver;
- ConfigFileReader configreader;
- String browser1;
+    private static WebDriver driver;
+    private static ConfigFileReader configreader;
+    private static String browser1;
 
 
- 
- public webDriverManager() {
-	 configreader= new ConfigFileReader();
-browser1=configreader.getBrowser();
+    public webDriverManager() {
+        configreader = new ConfigFileReader();
+        browser1 = configreader.getBrowser();
 
- }
- 
- public WebDriver getDriver() {
- if(driver == null)
-	 driver = createDriver();
- return driver;
- }
- 
+    }
 
- 
+    public static WebDriver getDriver() {
+        if (driver == null)
+            driver = createDriver();
+        return driver;
+    }
 
- 
- private WebDriver createDriver() {
-        switch (browser1) {     
-        case "firefox" : 
-        	driver = new FirefoxDriver();
-      break;
-        case "chrome" : 
-        	  System.setProperty("webdriver.chrome.driver",configreader.getchromeDriverPath());
-        	driver = new ChromeDriver();
-      break;
-      
-      
-    
-    
 
+    private static WebDriver createDriver() {
+        if ("firefox".equals(browser1)) {
+            driver = new FirefoxDriver();
+        } else if ("chrome".equals(browser1)) {
+            System.setProperty("webdriver.chrome.driver", configreader.getchromeDriverPath());
+            driver = new ChromeDriver();
         }
- 
-       driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(configreader.getImplicitlyWait(), TimeUnit.SECONDS);
- return driver;
- } 
- 
- public void closeDriver() {
 
- driver.quit();
- }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(configreader.getImplicitlyWait(), TimeUnit.SECONDS);
+        return driver;
+    }
+
+    private static final Thread CLOSE_THREAD = new Thread() {
+        @Override
+        public void run() {
+            try {
+                driver.quit();
+            } catch ( Exception e ) {
+                // e.printStackTrace();
+            }
+        }
+    };
+
+    static {
+        Runtime.getRuntime().addShutdownHook(CLOSE_THREAD);
+    }
 }
  
